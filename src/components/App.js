2d1a9b2291ec660e23f18278d289785577b0b131
Tish Chungoora <tishal82@gmail.com>
2019-12-16 08:13:21 +0000

    Implemented sorting functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,28 +6,43 @@ import hogs from "../porkers_data";
 
 export default class App extends Component {
   state = {
-    filtered: false
+    filtered: false,
+    sorted: null
   };
 
   setFiltered = () => {
     this.setState({
       filtered: !this.state.filtered
-    })
+    });
   };
 
-  filteredHog = () => {
-    const newHogs = [...hogs]
+  setSorted = (option) => {
+    this.setState({
+      sorted: option
+    });
+  };
+
+  filteredHogs = () => {
+    const newHogs = [...hogs];
     if (this.state.filtered) {
-      return newHogs.filter(hog => hog.greased)
+      return newHogs.filter(hog => hog.greased);
     }
-    return newHogs
-  }
+    return newHogs;
+  };
+
+  sortedHogs = () => {
+    const newHogs = [...this.filteredHogs()];
+    const { sorted } = this.state;
+    if (sorted === "name")
+      return newHogs.sort((a, b) => a.name.localeCompare(b.name));
+    return newHogs;
+  };
 
   render() {
     return (
       <div className="App">
-        <Nav setFiltered={this.setFiltered}/>
-        <HogList hogs={this.filteredHog()} />
+        <Nav setFiltered={this.setFiltered} setSorted={this.setSorted} />
+        <HogList hogs={this.sortedHogs()} />
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import piggy from "../porco.png";
 import React from "react";
 
-const Nav = (props) => {
+const Nav = props => {
   return (
     <div className="navWrapper">
       <span className="headerText">Hogwarts</span>
@@ -10,9 +10,8 @@ const Nav = (props) => {
       </div>
       <span className="normalText">A React App for County Fair Hog Fans</span>
       <p>
-        <button onClick={props.setFiltered}>
-          Filter greased porkies
-        </button>
+        <button onClick={props.setFiltered}>Filter Greased Porkies</button>
+        <button onClick={() => props.setSorted("name")}>Sort by Name</button>
       </p>
     </div>
   );
